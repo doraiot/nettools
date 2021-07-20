2feb2e44023a71e93d182cc6acfc28133a77b9c3
王春雷 <wangchunlei@dyrs.com.cn>
2021-07-20 14:20:25 +0800

    init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/howeyc/crc16"
+)
+
+func main() {
+	s := "EF129978000000"
+	data, err := hex.DecodeString(s)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("% X\n", data)
+	checksum := ^crc16.ChecksumIBM(data)
+	c := fmt.Sprintf("%04X", checksum)
+	fmt.Println(c[:2])
+	fmt.Println(c[2:])
+}
